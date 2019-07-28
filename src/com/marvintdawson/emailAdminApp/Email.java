fe1295100e2c64a10fdaa26ba35cd013c4a350cb
Marvin Dawson <marvintdawson@gmail.com>
2019-07-28 13:11:23 -0500

    updated the README.md file content

--- src/com/marvintdawson/emailAdminApp/Email.java
+++ src/com/marvintdawson/emailAdminApp/Email.java
@@ -3,6 +3,7 @@ package com.marvintdawson.emailAdminApp;
 public class Email {
     String firstName;
     String lastName;
+    String email;
     String department;
     String password;
     int mailBoxCapacity;
