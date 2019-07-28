bc3c799b98e4f2375a9c6feb3a7232b3456b860f
Marvin Dawson <marvintdawson@gmail.com>
2019-07-28 12:32:33 -0500

    initial commit

---
+++ src/com/marvintdawson/emailAdminApp/Email.java
@@ -0,0 +1,10 @@
+package com.marvintdawson.emailAdminApp;
+
+public class Email {
+    String firstName;
+    String lastName;
+    String department;
+    String password;
+    int mailBoxCapacity;
+    String alternateEmail;
+}
